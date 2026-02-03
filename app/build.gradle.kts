2703e0330cc411cc415cec9458a75abe2491110e
Vaidulla <vajdullaak2@gmail.com>
2026-02-03 09:37:35 +0300

    Отправка Лр

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+}
+
+android {
+    namespace = "com.example.vaidullalb1"
+
+    defaultConfig {
+        applicationId = "com.example.vaidullalb1"
+        minSdk = 31
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+}
